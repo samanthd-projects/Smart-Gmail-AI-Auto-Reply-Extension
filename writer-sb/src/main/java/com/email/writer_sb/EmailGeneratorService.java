b471855addcafbcd0f187547d25d61541c3e8905
ns4024647@gmail.com <samanthd2004@gmail.com>
2025-12-22 06:27:49 +0530

    added new

--- writer-sb/src/main/java/com/email/writer_sb/EmailGeneratorService.java
+++ writer-sb/src/main/java/com/email/writer_sb/EmailGeneratorService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
+//new
 
 
 import static org.springframework.web.servlet.function.RequestPredicates.path;
